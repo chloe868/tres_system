9fcb97dc0481f4508b1b8c4216ca27321ef99dbc
2ndyrGroupB <2ndyrGroupB@pn.local>
2020-01-24 15:02:44 +0800

    jan242020 hapon

--- pnc/routes/web.php
+++ pnc/routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'ScholarController@home')->name('home');
+Route::get('/dashboard/add', 'ScholarController@add_view')->name('add');
+Route::get('/dashboard/edit/{id}','ScholarController@edit')->name('edit');
+Route::post('/dashboard/store', 'ScholarController@store')->name('store');
+//Route::post('/dashboard/delete/{id?}','ScholarController@delete')->name('delete');
+Route::get('/dashboard/delete/{id?}','ScholarController@delete')->name('delete');
+Route::post('/dashboard/update/{id}','ScholarController@update')->name('update');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
